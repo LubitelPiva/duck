2964b80cc4c187a6b6f4f8466b869665c1a3ffcb
Maxim <savoninmv@gmail.com>
2019-12-07 15:52:06 +0500

    started scrt

---
+++ 07-12-19/animation/v1/animation/animation.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "TXLib.h"
+int main()
+{
+	int x = 500, y = 500, h, l;
+	txCreateWindow(1000, 1000);
+	txClear();
+	txSetColor(TX_BLACK);
+	txRectangle(x - 20, y - 20, x + 20, y + 20);
+
+}
