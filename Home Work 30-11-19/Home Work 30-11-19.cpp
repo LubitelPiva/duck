938e5bc5c2e1b5bc400894f61e3f950b9a528c5e
Maxim <savoninmv@gmail.com>
2019-12-07 15:21:40 +0500

    home

---
+++ Home Work 30-11-19/Home Work 30-11-19.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "TXLib.h"
+
+int main()
+{
+	int x, y, R = 30, h = 60, l = 30, l1 = 35, s = 40, s1 = 30, h1 = 15;
+	cin >> x >> y;
+	txCreateWindow (500, 500);
+	txCircle (x, y, R);
+	txLine (x, y + R, x, y + R + h);
+	txLine(x, y + R + h, x - l, y + R + h + s);
+	txLine(x, y + R + h, x + l, y + R + h + s);
+	txLine(x, y + R + h1, x - l1, y + R + h1 + s1);
+	txLine(x, y + R + h1, x + l1, y + R + h1 + s1);
+}	
