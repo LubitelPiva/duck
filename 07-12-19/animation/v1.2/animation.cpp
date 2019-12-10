c00b574ae1a260432c421ff31ceeb029e58e25db
Maxim <savoninmv@gmail.com>
2019-12-10 20:50:40 +0500

    First Commit

--- 07-12-19/animation/v1.2/animation.cpp
+++ 07-12-19/animation/v1.2/animation.cpp
@@ -4,33 +4,19 @@
 using namespace std;
 void Draw_main_person()
 {
-	txSetColor(TX_BLACK);
-	txSetFillColor(RGB(0, 255, 0));
-	txRectangle(x_m - l, y_m - h, x_m + l, y_m + h);
+	
 }
 void Control_main_person()
 {
-	if (GetAsyncKeyState(VK_LEFT)) x_m -= 5;
-	if (GetAsyncKeyState(VK_RIGHT)) x_m += 5;
-	if (GetAsyncKeyState(VK_DOWN)) y_m += 5;
-	if (GetAsyncKeyState(VK_UP)) y_m -= 5;
-	if (x_m > 719) x_m = -19;
-	else if (x_m < -19) x_m = 719;
-	if (y_m > 729) y_m = -29;
-	else if (y_m < -29) y_m = 729;
+	
 }
 void Draw_stalker_person()
 {
-	txSetColor(TX_BLACK);
-	txSetFillColor(RGB(255, 0, 0));
-	txRectangle(x_s - l, y_s - h, x_s + l, y_s + h);
+	
 }
 void Control_stalker_person()
 {
-	if (x_s - x_m < 0) x_s += 5;
-	else if (x_s - x_m > 0) x_s -= 5;
-	if (y_s - y_m < 0) y_s += 5;
-	else if (y_s - y_m > 0) y_s -= 5;
+	
 }
 
 int main()
@@ -41,8 +27,29 @@ int main()
 	{
 		txSetFillColor(TX_WHITE);
 		txClear();
-		Draw_main_person();
-		Control_main_person();
+
+		txSetColor(TX_BLACK);
+		txSetFillColor(RGB(0, 255, 0));
+		txRectangle(x_m - l, y_m - h, x_m + l, y_m + h);
+
+		if (GetAsyncKeyState(VK_LEFT)) x_m -= 5;
+		if (GetAsyncKeyState(VK_RIGHT)) x_m += 5;
+		if (GetAsyncKeyState(VK_DOWN)) y_m += 5;
+		if (GetAsyncKeyState(VK_UP)) y_m -= 5;
+		if (x_m > 719) x_m = -19;
+		else if (x_m < -19) x_m = 719;
+		if (y_m > 729) y_m = -29;
+		else if (y_m < -29) y_m = 729;
+
+		txSetColor(TX_BLACK);
+		txSetFillColor(RGB(255, 0, 0));
+		txRectangle(x_s - l, y_s - h, x_s + l, y_s + h);
+		
+		if (x_s - x_m < 0) x_s += 5;
+		else if (x_s - x_m > 0) x_s -= 5;
+		if (y_s - y_m < 0) y_s += 5;
+		else if (y_s - y_m > 0) y_s -= 5;
+		
 		Sleep(50);
 		if (GetAsyncKeyState(VK_ESCAPE)) break;
 	}
