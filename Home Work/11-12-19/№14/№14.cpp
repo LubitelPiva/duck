a903a6846f731e5b48fec2388bd84bf1c0b5aa34
Maxim <savoninmv@gmail.com>
2019-12-11 11:46:15 +0500

    Home Worck

---
+++ Home Work/11-12-19/№14/№14.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cin >> a;
+	a = a / 100 * 64 + a / 10 % 10 * 8 + a % 10;
+	cout << a;
+}
---
+++ Home Work/11-12-19/№15/№15.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int x;
+void math_16()
+{
+    if (x == 10) cout << 'A';
+    else if (x == 11) cout << 'B';
+    else if (x == 12) cout << 'C';
+    else if (x == 13) cout << 'D';
+    else if (x == 14) cout << 'E';
+    else if (x == 15) cout << 'F';
+    else cout << x;
+}
+int main()
+{
+    int a;
+    cin >> a;
+    x = a / 16;
+    math_16();
+    x = a % 16;
+    math_16();
+}
