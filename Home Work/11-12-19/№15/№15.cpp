90919788b106839450cda12e7652474d1a8bd178
Your Name <you@example.com>
2019-12-11 18:58:41 +0500

    clear

--- Home Work/11-12-19/№15/№15.cpp
+++ Home Work/11-12-19/№15/№15.cpp
@@ -1,8 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
-int x;
-void math_16()
+
+char math_16(int x)
 {
+    if (x < 10) {
+        return char('0' + x);
+    } else {
+        return char('A' + x - 10);
+    }
+
     if (x == 10) cout << 'A';
     else if (x == 11) cout << 'B';
     else if (x == 12) cout << 'C';
@@ -13,9 +19,14 @@ void math_16()
 }
 int main()
 {
+    int x;
     int a;
     cin >> a;
+    cout << math_16(a / 16) << math_16(a % 16);
+    return 0;
     x = a / 16;
+
+
     math_16();
     x = a % 16;
     math_16();
