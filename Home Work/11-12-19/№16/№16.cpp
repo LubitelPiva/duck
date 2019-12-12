b46451480bae3857b8f23633d6d803a7392abaa9
LubitelPiva <57619285+LubitelPiva@users.noreply.github.com>
2019-12-12 16:11:36 +0500

    16

--- Home Work/11-12-19/№16/№16.cpp
+++ Home Work/11-12-19/№16/№16.cpp
@@ -1,19 +1,15 @@
-includes <iostream>
+#include <iostream>
 using namespace std;
-int sixten(char a, int b)
+int sixten(char a)
 {
     if (a < 10) return (a);
     else return (a - 'A' + 10);
-    /*if (a[a.size() - b] = 'a') c += 10;
-    else if (a[a.size() - b] = 'b') c += 11;
-    else if (a[a.size() - b] = 'c') c += 12;
-    else if (a[a.size() - b] = 'd') c += 13;
-    else if (a[a.size() - b] = 'e') c += 14;
-    else if (a[a.size() - b] = 'f') c += 15;*/
 }
 int main()
 {
-    char a;
-    cin >> a;
-    c += sixten(a[a.size() - 1]) + sixten(a[a.size() - 2]);
+    char a, b;
+    int c = 0;
+    cin >> a >> b;
+    c += sixten(a) * 16 + sixten(b);
+    cout << c;
 }
