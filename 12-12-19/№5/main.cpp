ae7cc91f33f00611f5925e8ba360177a79f8d8f0
Your Name <you@example.com>
2019-12-14 09:21:33 +0500

    work

--- 12-12-19/№5/main.cpp
+++ 12-12-19/№5/main.cpp
@@ -7,7 +7,7 @@ int main()
     int k;
     for (int i = 100; i < 1000; i++)
     {
-        k = (i * i) / 1000;
-        if (i == k) cout << i;
+        k = (i * i) % 1000;
+        if (i == k) cout << i << endl;
     }
 }
