5987b7db03232b6b1d1cdc29d4dbf24bc51227a4
Your Name <you@example.com>
2019-12-18 18:59:58 +0500

    kfkfs

---
+++ 07-12-19/animation/animationCB/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+#include "TXLib.h"
+using namespace std;
+int main()
+{
+    cout << "fdsa";
+}
--- 07-12-19/animation/v1.2/animation.cpp
+++ 07-12-19/animation/v1.2/animation.cpp
@@ -5,13 +5,12 @@ using namespace std;
 int x_m = 350, y_m = 350, h = 30, l = 20, x_s = 50, y_s = 50, N = 0, n = 1;
 void Draw_main_person()
 {
-	
+	txSetColor(TX_BLACK);
+    txSetFillColor(RGB(0, 255, 0));
+    txRectangle(x_m - l, y_m - h, x_m + l, y_m + h);
 }
 void Control_main_person()
 {
-<<<<<<< HEAD
-	
-=======
 	if (GetAsyncKeyState(VK_LEFT)) {x_m -= 5; n = 4;}
 	if (GetAsyncKeyState(VK_RIGHT)) {x_m += 5; n = 2;}
 	if (GetAsyncKeyState(VK_DOWN)) {y_m += 5; n = 3;}
@@ -21,19 +20,19 @@ void Control_main_person()
 	else if (x_m < -19) x_m = 719;
 	if (y_m > 729) y_m = -29;
 	else if (y_m < -29) y_m = 729;
->>>>>>> bcb2202cd090e2d3268c8d9430827d2eea1800e1
 }
 void Draw_stalker_person()
 {
-	
+	txSetColor(TX_BLACK);
+    txSetFillColor(RGB(255, 0, 0));
+    txRectangle(x_s - l, y_s - h, x_s + l, y_s + h);
 }
 void Control_stalker_person()
 {
-	
-}
-void drawball()
-{
-    int x_b, y_b;
+	if (x_s - x_m < 0) x_s += 5;
+    else if (x_s - x_m > 0) x_s -= 5;
+    if (y_s - y_m < 0) y_s += 5;
+    else if (y_s - y_m > 0) y_s -= 5;
 }
 int main()
 {
@@ -42,36 +41,10 @@ int main()
 	{
 		txSetFillColor(TX_WHITE);
 		txClear();
-<<<<<<< HEAD
-
-		txSetColor(TX_BLACK);
-		txSetFillColor(RGB(0, 255, 0));
-		txRectangle(x_m - l, y_m - h, x_m + l, y_m + h);
-
-		if (GetAsyncKeyState(VK_LEFT)) x_m -= 5;
-		if (GetAsyncKeyState(VK_RIGHT)) x_m += 5;
-		if (GetAsyncKeyState(VK_DOWN)) y_m += 5;
-		if (GetAsyncKeyState(VK_UP)) y_m -= 5;
-		if (x_m > 719) x_m = -19;
-		else if (x_m < -19) x_m = 719;
-		if (y_m > 729) y_m = -29;
-		else if (y_m < -29) y_m = 729;
-
-		txSetColor(TX_BLACK);
-		txSetFillColor(RGB(255, 0, 0));
-		txRectangle(x_s - l, y_s - h, x_s + l, y_s + h);
-		
-		if (x_s - x_m < 0) x_s += 5;
-		else if (x_s - x_m > 0) x_s -= 5;
-		if (y_s - y_m < 0) y_s += 5;
-		else if (y_s - y_m > 0) y_s -= 5;
-		
-=======
 		Draw_main_person();
 		Control_main_person();
 		Draw_stalker_person();
 		Control_stalker_person();
->>>>>>> bcb2202cd090e2d3268c8d9430827d2eea1800e1
 		Sleep(50);
 		if (GetAsyncKeyState(VK_ESCAPE)) break;
 	}
