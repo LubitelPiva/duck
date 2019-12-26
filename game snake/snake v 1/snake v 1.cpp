f5474821cfbefb9a716af5b7387fc2d83d63d5ba
wwhitedeer <pilyugina.03@mail.ru>
2019-12-26 19:00:48 +0500

    snake

--- game snake/snake v 1/snake v 1.cpp
+++ game snake/snake v 1/snake v 1.cpp
@@ -1,9 +1,15 @@
 ﻿#include <iostream>
 #include "TXLib.h"
 using namespace std;
-class snake 
+int N = 4;
+class snake
 {
 public:
+    snake ()
+    {
+        x = 20;
+        y = 20;
+    }
 	void print_squb()
 	{
 		txSetFillColor(RGB(0, 255, 0));
@@ -17,6 +23,22 @@ public:
 		else if (GetAsyncKeyState(VK_LEFT)) x -= 40;
 		while (GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState(VK_DOWN) || GetAsyncKeyState(VK_LEFT)) {}
 	}
+	void SetX (int Setx)
+	{
+	    x = Setx;
+	}
+	void SetY (int Sety)
+	{
+	    y = Sety;
+	}
+	int GetX ()
+	{
+	    return x;
+	}
+	int GetY ()
+	{
+	    return y;
+	}
 private:
 	int x = 300;
 	int y = 300;
@@ -28,17 +50,41 @@ private:
 }*/
 int main()
 {
-	int win_h = 600, win_l = 600, x = 360, y = 360, N = 1;
+	int win_h = 600, win_l = 600, x = 360, y = 360;
 	while (!GetAsyncKeyState(VK_RETURN)) {}
 	txCreateWindow(win_h, win_l);
-	snake a;
+	snake All[N];
 	while (true)
 	{
 		if (GetAsyncKeyState(VK_ESCAPE)) break;
-		txSetFillColor(RGB(255, 255, 255));
+		for (int i = 0; i < N; i++)
+        {
+            All[i].print_squb();
+        }
+		if (GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState(VK_DOWN) || GetAsyncKeyState(VK_LEFT))
+        {
+            for (int i = 1; i < N; i++)
+            {
+                All[i].SetX(All[i - 1].GetX());
+                All[i].SetY(All[i - 1].GetY());
+            }
+            All[0].Control_snake();
+        }
+        txSetFillColor(RGB(255, 255, 255));
 		txClear();
-		a.print_squb();
-		a.Control_snake();
 		Sleep(50);
 	}
 }
+/*void go_snake(snake &All)
+	{
+	    if (GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState(VK_DOWN) || GetAsyncKeyState(VK_LEFT))
+        {
+            for (int i = 1; i < N; i++)
+            {
+                All[i].SetX(snake[i-1].GetX());
+                All[i].SetY(snake[i-1].GetY());
+            }
+            All[0].Control_snake();
+        }
+	}
+*/
