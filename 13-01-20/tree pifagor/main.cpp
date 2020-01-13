da6fc8aef588e1f97a49da6fe1ca091b7ffc0d34
wwhitedeer <pilyugina.03@mail.ru>
2020-01-13 15:36:17 +0500

    work

---
+++ 13-01-20/tree pifagor/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "TXLib.h"
+using namespace std;
+int color[3];
+
+void Tree( float x, float y, float L, float angle, int levels)
+{
+    if ( levels <= 0 ) return;
+    color[0] = color[0] + 10;
+    if (color[0] > 255) color[1] = color[1] + 20;
+    if (color[1] > 255) color[2] = color[2] + 30;
+    const float k = 0.8;
+    const float angle1 = angle - 60;
+    const float angle2 = angle + 60;
+    float x1 = x + L * cos ( angle * M_PI / 180);
+    float y1 = y - L * sin ( angle * M_PI / 180);
+    txSetColour(RGB(color[0], color[1], color[2]));
+    txLine(x, y ,x1 , y1);
+    Tree ( x1, y1, k * L, angle, levels - 1);
+    Tree ( x1, y1, k * L, angle1, levels - 1);
+    Tree ( x1, y1, k * L, angle2, levels - 1);
+}
+int main()
+{
+    color[0] = 50;
+    color[1] = 20;
+    color[2] = 20;
+    txCreateWindow(1000, 1000);
+    Tree(500, 800, 200, 90, 10);
+}
